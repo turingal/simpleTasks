5c185512f15b094a8fe643828cfde6a807ebeaeb
turingal <wonder1779@mail.ru>
2022-09-21 15:18:56 +0300

    make abbreviation for string task

---
+++ abbreviation.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	phrase := readString()
+	words := strings.Fields(phrase)
+	var abbr []rune
+
+	for _, word := range words {
+		letter := []rune(word)[0]
+
+		if !unicode.IsLetter(letter) {
+			continue
+		}
+
+		abbr = append(abbr, unicode.ToUpper(letter))
+
+	}
+
+	fmt.Println(string(abbr))
+}
+
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
